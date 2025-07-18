fd637199fd78640351d1a4f0f5ba390383f3f743
Nate M <codebynate@gmail.com>
2025-07-18 20:31:04 +0100

    Start setting up routing for Home and Country detail pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App.jsx'
 import './index.css'
-import SingleCountry from './pages/SingleCountry.jsx'
+import Home from './pages/Home'
+import Country from './pages/Country.jsx'
 
 const root = document.getElementById('root')
 
 ReactDOM.createRoot(root).render(
     <BrowserRouter>
         <Routes>
-            <Route index element={<App />} />
-            <Route path=":country" element={<SingleCountry />} />
+            <Route element={<App />}>
+                <Route index element={<Home />} />
+                <Route path=":slug" element={<Country />} />
+            </Route>
         </Routes>
     </BrowserRouter>
 )
